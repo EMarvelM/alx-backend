785d4c1edda1cf907e962c8f351852baa5f34c24
EMarvelM <marvelousegbe19@gmail.com>
2024-07-23 12:23:28 +0100

    more efficient

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,30 +39,31 @@ class Server:
         assert(page > 0 and page_size > 0)
         i = index_range(page, page_size)
         return self.dataset()[i[0]: i[1]]
-    
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Get the page of the dataset
+        """
+        Get the page of the dataset
 
         Args:
             page (int, optional): Page number. Defaults to 1.
             page_size (int, optional): Size of the page. Defaults to 10.
 
-        Returns:
-            List[List]: List of the dataset
+            Returns:
+                List[List]: List of the dataset
         """
         assert(type(page) is int and type(page_size) is int)
         assert(page > 0 and page_size > 0)
         self.dataset()
         i = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
 
         return {
             "page_size": page_size,
             "page": page,
             "data": self.dataset()[i[0]: i[1]],
-            "next_page": page + 1 if i[1] + 1 < int(len(self.__dataset) / page_size) else None,
-            "prev_page": page - 1 if  i[0] - 1 < int(len(self.__dataset) / page_size) else None,
-            "total_pages": int(len(self.__dataset) / page_size)
+            "next_page": page + 1 if i[1] + 1 < total_pages else None,
+            "prev_page": page - 1 if i[0] - 1 < total_pages else None,
+            "total_pages": total_pages
         }
 
 
